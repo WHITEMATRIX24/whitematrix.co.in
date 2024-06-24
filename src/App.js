18ff016c667efaae2ed1e60ddf578a0d3b69e32e
johnathikalam2 <137830315+johnathikalam2@users.noreply.github.com>
2024-06-24 18:34:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+// App.js or index.js
+import React from 'react';
+import './components/style.css';
+import Career from './components/career';
+import Insight from './components/insight';
+import MyComponent from './components/home_screen';
+import MobileApp from './components/slider/mobileapp';
+import Software from './components/slider/software';
+import Schedule from './components/scheduleconvo';
+import Voip from './components/slider/voip';
+import AI from './components/slider/ai';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/wm-portfolio" element={<MyComponent />} />
+          <Route path="/career" element={<Career />} />
+          <Route path="/insight" element={<Insight />} />
+          <Route path="/mobile" element={<MobileApp />} />
+          <Route path="/software" element={<Software />} />
+          <Route path="/schedule" element={<Schedule />} />
+          <Route path="/voip" element={<Voip />} />
+          <Route path="/ai" element={<AI />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
