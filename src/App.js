c4b701e0e69341e0c84c07c48e76d6800042a919
Adwaith Jayasankar <kichu31084@gmail.com>
2024-07-03 17:19:30 +0530

    First deployment

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-// App.js or index.js
-import React from 'react';
-import './components/style.css';
-import Career from './components/career';
-import Insight from './components/insight';
-import MyComponent from './components/home_screen';
-import MobileApp from './components/slider/mobileapp';
-import Software from './components/slider/software';
-import Schedule from './components/scheduleconvo';
-import Voip from './components/slider/voip';
-import AI from './components/slider/ai';
+import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MyComponent from './home_screen';
+import Career from './career';
+import MobileApp from './mobileapp';
+import Software from './software';
+import Schedule from './scheduleconvo';
+import Voip from './voip';
+import AI from './ai';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/whitematrix.co.in" element={<MyComponent />} />
-          <Route path="/career" element={<Career />} />
-          <Route path="/insight" element={<Insight />} />
-          <Route path="/mobile" element={<MobileApp />} />
-          <Route path="/software" element={<Software />} />
-          <Route path="/schedule" element={<Schedule />} />
-          <Route path="/voip" element={<Voip />} />
-          <Route path="/ai" element={<AI />} />
-        </Routes>
+    <div><Router>
+      <Routes>
+        <Route path='/' element={<MyComponent/>}/>
+        <Route path="/career" element={<Career />} />
+        <Route path="/mobile" element={<MobileApp />} />
+        <Route path="/software" element={<Software />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/voip" element={<Voip />} />
+        <Route path="/ai" element={<AI />} />
+      </Routes>
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
